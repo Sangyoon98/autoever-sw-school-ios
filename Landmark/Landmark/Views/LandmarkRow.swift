d92c0eb67e0a79b53d03199415902ba37b65ee56
Sangyoon98 <qlrqod123123@naver.com>
2025-08-18 17:39:13 +0900

    2025-08-18

--- Landmark/Landmark/Views/LandmarkRow.swift
+++ Landmark/Landmark/Views/LandmarkRow.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
     }
 }
 
-#Preview {
-    LandmarkRow()
+#Preview("그룹") {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
+
+#Preview("N서울타워") {
+    LandmarkRow(landmark: landmarks[0])
+}
+
+#Preview("경복궁") {
+    LandmarkRow(landmark: landmarks[1])
 }
