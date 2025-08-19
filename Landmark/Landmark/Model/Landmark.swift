000763f0d41368204a4b44f0562da75ce4e5a8e0
Sangyoon98 <qlrqod123123@naver.com>
2025-08-19 17:50:18 +0900

    2025-08-19

--- Landmark/Landmark/Model/Landmark.swift
+++ Landmark/Landmark/Model/Landmark.swift
@@ -12,9 +12,20 @@ import CoreLocation
 struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
+    var city: String
     var park: String
     var state: String
     var description: String
+    var isFavorite: Bool
+    var isFeatured: Bool
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case towers = "Towers"
+        case palaces = "Palaces"
+        case mountains = "Mountains"
+        case temples = "Temples"
+    }
     
     private var imageName: String
     var image: Image {  // 즉시 실행 클로저
--- Landmark/Landmark/Model/ModelData.swift
+++ Landmark/Landmark/Model/ModelData.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.json")
+@Observable
+class ModelData {
+    // @Published
+    var landmarks: [Landmark] = load("landmarkData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+}
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
@@ -15,14 +30,14 @@ func load<T: Decodable>(_ filename: String) -> T {
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
-        fatalError("Couldn't find (filename) in main bundle.")
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
 
 
     do {
         data = try Data(contentsOf: file)
     } catch {
-        fatalError("Couldn't load (filename) from main bundle:\n(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\n(error)")
     }
 
 
@@ -30,6 +45,6 @@ func load<T: Decodable>(_ filename: String) -> T {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
-        fatalError("Couldn't parse (filename) as (T.self):\n(error)")
+        fatalError("Couldn't parse \(filename) as (T.self):\n(error)")
     }
 }
