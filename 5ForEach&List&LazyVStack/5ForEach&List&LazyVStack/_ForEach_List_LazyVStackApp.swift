7ad0388f556a7ac06cf4c35900758c131012edcd
Sangyoon98 <qlrqod123123@naver.com>
2025-08-19 09:47:24 +0900

    2025-08-19

---
+++ 5ForEach&List&LazyVStack/5ForEach&List&LazyVStack/_ForEach_List_LazyVStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ForEach_List_LazyVStackApp.swift
+//  5ForEach&List&LazyVStack
+//
+//  Created by 채상윤 on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct _ForEach_List_LazyVStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
